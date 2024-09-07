622ac1ef3cc747230847b786797eb333ac6f8a60
Shubham Kumar <johndoe@email.com>
2024-09-08 00:49:20 +0530

    Commit 1

--- backend/utils/lowerCaseExceptPassword.js
+++ backend/utils/lowerCaseExceptPassword.js
@@ -2,9 +2,7 @@ function lowerCaseExceptPassword(params) {
   let updatedParams = {};
 
   for (let key in params) {
-    if (typeof params[key] === "Number") {
-      updatedParams[key] = params[key];
-    } else if (key !== "password" && typeof params[key] === "string") {
+    if (key !== "password" && typeof params[key] === "string") {
       updatedParams[key] = params[key].toLowerCase();
     } else {
       updatedParams[key] = params[key];
